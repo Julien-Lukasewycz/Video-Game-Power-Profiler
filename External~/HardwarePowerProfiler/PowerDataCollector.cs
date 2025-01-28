23fc0699d523fcd0afbb4b848bf56def974f5de2
Julien Lukasewycz <julien.lukasewycz@uni-due.de>
2025-01-28 14:45:17 +0100

    [ADDED] PowerDataCollector class implementation

---
+++ External~/HardwarePowerProfiler/PowerDataCollector.cs
@@ -0,0 +1,36 @@
+﻿using LibreHardwareMonitor.Hardware;
+
+namespace HardwarePowerProfiler;
+
+internal class PowerDataCollector
+{
+    private readonly Computer computer = new()
+    {
+        IsBatteryEnabled = true,
+        IsControllerEnabled = true,
+        IsCpuEnabled = true,
+        IsGpuEnabled = true,
+        IsMemoryEnabled = true,
+        IsMotherboardEnabled = true,
+        IsNetworkEnabled = true,
+        IsPsuEnabled = true,
+        IsStorageEnabled = true
+    };
+    private readonly UpdateVisitor updateVisitor = new();
+
+    internal void StartCollecting()
+    {
+        computer.Open();
+    }
+
+    internal void GetData()
+    {
+        computer.Accept(updateVisitor);
+        // TODO
+    }
+
+    internal void StopCollecting()
+    {
+        computer.Close();
+    }
+}
