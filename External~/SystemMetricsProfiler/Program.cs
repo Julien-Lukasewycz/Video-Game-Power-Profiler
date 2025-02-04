f2d364aab0aed6a03c7264aed8e13e3315c4cb1d
Julien Lukasewycz <julien.lukasewycz@uni-due.de>
2025-02-04 13:27:26 +0100

    [ADDED] initial code for SystemMetricsProfiler

---
+++ External~/SystemMetricsProfiler/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Diagnostics;
+
+namespace SystemMetricsProfiler;
+
+public static class Program
+{
+    private static void Main(string[] args)
+    {
+        Process process = Process.GetProcessById(int.Parse(args[0]));
+        process.Exited += ProcessExited;
+        GetProcessMetrics(process);
+    }
+
+    private static void ProcessExited(object? sender, EventArgs e)
+    {
+        Environment.Exit(0);
+    }
+
+    private static void GetProcessMetrics(Process process)
+    {
+        // TODO
+    }
+}
