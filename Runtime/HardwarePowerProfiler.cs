b314372db4ebbb8b272c844f65bf55533320bd97
Julien Lukasewycz <julien.lukasewycz@uni-due.de>
2025-02-11 14:12:17 +0100

    [ADDED] Code to start HardwarePowerProfiler from inside game

---
+++ Runtime/HardwarePowerProfiler.cs
@@ -0,0 +1,24 @@
+using System.Diagnostics;
+using UnityEngine;
+
+namespace PowerProfiler.Runtime
+{
+    internal class HardwarePowerProfiler : MonoBehaviour
+    {
+        private readonly Process process = new();
+        
+        private void Awake()
+        {
+            process.StartInfo.FileName = "Assets\\Power Profiler\\External~\\HardwarePowerProfiler\\bin\\Release\\net8.0\\HardwarePowerProfiler.exe"; // TODO use published files
+            process.StartInfo.UseShellExecute = true;
+            process.StartInfo.Verb = "runas";
+            process.Start();
+            Application.quitting += StopProcess;
+        }
+
+        private void StopProcess()
+        {
+            process.Kill();
+        }
+    }
+}
