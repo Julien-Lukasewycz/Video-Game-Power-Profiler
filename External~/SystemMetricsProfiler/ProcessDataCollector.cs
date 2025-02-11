dd3a9c970e2249af7b4ba055a9b186261a446f74
Julien Lukasewycz <julien.lukasewycz@uni-due.de>
2025-02-11 16:36:30 +0100

    [ADDED] code to retrieve process total cpu utilization to ProcessDataCollector

---
+++ External~/SystemMetricsProfiler/ProcessDataCollector.cs
@@ -0,0 +1,42 @@
+﻿using System.Diagnostics;
+using System.Runtime.Versioning;
+
+namespace SystemMetricsProfiler;
+
+internal class ProcessDataCollector(Process process)
+{
+    [SupportedOSPlatform("windows")]
+    private readonly PerformanceCounter cpuCounter = new("Process", "% Processor Time", GetProcessInstanceName(process.Id), true);
+    private UtilizationData utilizationData = new();
+    
+    [SupportedOSPlatform("windows")]
+    internal void StartCollecting()
+    {
+        _ = cpuCounter.NextValue();
+        // TODO
+        Thread.Sleep(1000);
+    }
+
+    [SupportedOSPlatform("windows")]
+    internal UtilizationData GetData()
+    {
+        utilizationData.CpuCores.Clear();
+        utilizationData.Cpu = cpuCounter.NextValue() / Environment.ProcessorCount;
+        // TODO
+        return utilizationData;
+    }
+    
+    [SupportedOSPlatform("windows")]
+    private static string GetProcessInstanceName(int processId)
+    {
+        foreach (string? instance in new PerformanceCounterCategory("Process").GetInstanceNames())
+        {
+            PerformanceCounter counter = new("Process", "ID Process", instance, true);
+            if ((int)counter.NextValue() == processId)
+            {
+                return instance;
+            }
+        }
+        throw new Exception("Process instance not found");
+    }
+}
