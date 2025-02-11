1213647c4470e7ec4e655470f38355168158bbd4
Julien Lukasewycz <julien.lukasewycz@uni-due.de>
2025-02-11 12:54:52 +0100

    [ADDED] code to read our hardware power data to PowerDataCollector

--- External~/HardwarePowerProfiler/PowerData.cs
+++ External~/HardwarePowerProfiler/PowerData.cs
@@ -2,5 +2,16 @@
 
 internal struct PowerData
 {
-    // TODO
+    internal List<float> CpuCores = [];
+    internal float CpuPackage = 0;
+    internal float CpuPPT = 0;
+    internal float CpuCorePower = 0;
+    internal float CpuSocPower = 0;
+    internal float CpuMiscPower = 0;
+    internal float CpuTotalPower = 0;
+    internal float IGpuCore = 0;
+    internal float IGpuSoc = 0;
+    internal float GpuPackage = 0;
+
+    public PowerData() {}
 }
--- External~/HardwarePowerProfiler/PowerDataCollector.cs
+++ External~/HardwarePowerProfiler/PowerDataCollector.cs
@@ -17,6 +17,7 @@ internal class PowerDataCollector
         IsStorageEnabled = true
     };
     private readonly Dictionary<string, int[]> sensorPositions = new();
+    private PowerData powerData = new();
     private readonly UpdateVisitor updateVisitor = new();
 
     internal void StartCollecting()
@@ -42,10 +43,25 @@ internal class PowerDataCollector
         sensorPositions.Add("GPU Package", [gpuPosition, Array.FindIndex(computer.Hardware[gpuPosition].Sensors, sensor => sensor.Name == "GPU Package")]);
     }
 
-    internal void GetData()
+    internal PowerData GetData()
     {
+        powerData.CpuCores.Clear();
         computer.Accept(updateVisitor);
-        // TODO
+        for (int i = 1; i <= 16; i++)
+        {
+            string name = $"Core #{i} (SMU)";
+            powerData.CpuCores.Add(computer.Hardware[sensorPositions[name][0]].Sensors[sensorPositions[name][1]].Value ?? 0);
+        }
+        powerData.CpuPackage = computer.Hardware[sensorPositions["Package"][0]].Sensors[sensorPositions["Package"][1]].Value ?? 0;
+        powerData.CpuPPT = computer.Hardware[sensorPositions["CPU PPT"][0]].Sensors[sensorPositions["CPU PPT"][1]].Value ?? 0;
+        powerData.CpuCorePower = computer.Hardware[sensorPositions["Core Power"][0]].Sensors[sensorPositions["Core Power"][1]].Value ?? 0;
+        powerData.CpuSocPower = computer.Hardware[sensorPositions["SOC Power"][0]].Sensors[sensorPositions["SOC Power"][1]].Value ?? 0;
+        powerData.CpuMiscPower = computer.Hardware[sensorPositions["Misc Power"][0]].Sensors[sensorPositions["Misc Power"][1]].Value ?? 0;
+        powerData.CpuTotalPower = computer.Hardware[sensorPositions["Total Power"][0]].Sensors[sensorPositions["Total Power"][1]].Value ?? 0;
+        powerData.IGpuCore = computer.Hardware[sensorPositions["GPU Core"][0]].Sensors[sensorPositions["GPU Core"][1]].Value ?? 0;
+        powerData.IGpuSoc = computer.Hardware[sensorPositions["GPU SoC"][0]].Sensors[sensorPositions["GPU SoC"][1]].Value ?? 0;
+        powerData.GpuPackage = computer.Hardware[sensorPositions["GPU Package"][0]].Sensors[sensorPositions["GPU Package"][1]].Value ?? 0;
+        return powerData;
     }
 
     internal void StopCollecting()
