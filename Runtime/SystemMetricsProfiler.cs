f2a06588f3244991c216c0148ebf0c86157d1df9
Julien Lukasewycz <julien.lukasewycz@uni-due.de>
2025-02-11 15:24:07 +0100

    [ADDED] SystemMetricsProfiler auto-start code

---
+++ Runtime/SystemMetricsProfiler.cs
@@ -0,0 +1,24 @@
+using System.Diagnostics;
+using UnityEngine;
+
+namespace PowerProfiler.Runtime
+{
+    internal class SystemMetricsProfiler : MonoBehaviour
+    {
+        private readonly Process process = new();
+        
+        private void Awake()
+        {
+            process.StartInfo.FileName = "Assets\\Power Profiler\\External~\\SystemMetricsProfiler\\bin\\Release\\net8.0\\SystemMetricsProfiler.exe"; // TODO use published files
+            process.StartInfo.UseShellExecute = true;
+            process.StartInfo.Verb = "runas";
+            process.Start();
+            Application.quitting += StopProcess;
+        }
+
+        private void StopProcess()
+        {
+            process.Kill();
+        }
+    }
+}
