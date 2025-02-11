bc2bc933443eb50e8e0c03ac0bc36c7757ea6710
Julien Lukasewycz <julien.lukasewycz@uni-due.de>
2025-02-11 15:16:20 +0100

    [ADDED] data collection and export to SystemMetricsProfiler

--- External~/SystemMetricsProfiler/Program.cs
+++ External~/SystemMetricsProfiler/Program.cs
@@ -6,16 +6,30 @@ namespace SystemMetricsProfiler;
 internal static class Program
 {
     [SupportedOSPlatform("windows")]
-    private static void Main(string[] args)
+    private static async Task Main(string[] args)
     {
         Process process = Process.GetProcessById(int.Parse(args[0]));
-        process.Exited += ProcessExited;
         ProcessDataCollector processDataCollector = new(process);
-        processDataCollector.ProfileProcessMetrics();
-    }
-
-    private static void ProcessExited(object? sender, EventArgs e)
-    {
-        Environment.Exit(0);
+        PeriodicTimer periodicTimer = new(TimeSpan.FromSeconds(1)); // TODO set measurement interval
+        Directory.CreateDirectory("Benchmark");
+        string path = $"Benchmark/ProcessUtilizationData {DateTime.Now:dd-MM-yyyy HH-mm-ss}.csv";
+        string content = "Time;";
+        for (int i = 1; i <= 16; i++)
+        {
+            content += $"CpuCoreUtilization{i};";
+        }
+        content += "CpuUtilization\r\n";
+        await File.AppendAllTextAsync(path, content);
+        while (await periodicTimer.WaitForNextTickAsync())
+        {
+            UtilizationData utilizationData = processDataCollector.GetData();
+            content = $"{DateTime.Now:dd:MM:yyyy HH:mm:ss:fff};";
+            foreach (float cpuCoreUtilization in utilizationData.CpuCores)
+            {
+                content += $"{cpuCoreUtilization};";
+            }
+            content += $"{utilizationData.Cpu};\r\n";
+            await File.AppendAllTextAsync(path, content);
+        }
     }
 }
