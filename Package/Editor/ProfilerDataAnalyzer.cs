872eeb0803cb887523f40b1ece4cd10d1ea5c2f3
Julien Lukasewycz <julien.lukasewycz@uni-due.de>
2025-02-25 16:03:26 +0100

    [ADDED] custom categories to ProfilerDataAnalyzer

--- Package/Editor/ProfilerDataAnalyzer.cs
+++ Package/Editor/ProfilerDataAnalyzer.cs
@@ -15,48 +15,91 @@ namespace PowerProfiler.Editor
         private static List<string> Files = new();
         private static int FileIndex;
         private static string Path;
-        private static bool HeaderWritten;
         private static int FrameIndex;
+        private static readonly Dictionary<string, int> CategoryMapping = new()
+        {
+            { "Virtual Texturing", 0 },
+            { "Audio Update Job", 1 },
+            { "Ai", 6 },
+            { "Loading", 8 },
+            { "UI Layout", 3 },
+            { "Scripts", 7 },
+            { "Other", 9 },
+            { "Input", 12 },
+            { "Text", 3 },
+            { "Lighting", 4 },
+            { "Physics", 2 },
+            { "Particles", 5 },
+            { "UI Details", 3 },
+            { "Debug", 7 },
+            { "GC", 9 },
+            { "Burst Jobs", 7 },
+            { "GPU", 0 },
+            { "Director", 11 },
+            { "Vr", 0 },
+            { "Animation", 11 },
+            { "Overhead", 9 },
+            { "Managed Jobs", 7 },
+            { "PlayerLoop", 7 },
+            { "Audio Job", 1 },
+            { "Network", 8 },
+            { "Build Interface", 9 },
+            { "FileIO", 8 },
+            { "Physics2D", 2 },
+            { "Video", 10 },
+            { "Render", 0 },
+            { "Audio", 1 },
+            { "VSync", 0 },
+            { "Jobs", 7 },
+            { "Gui", 3 },
+            { "Internal", 9 },
+            { "UI Render", 3 },
+            { "VFX", 5 },
+            { "Memory", 9 },
+            { "Network Operations", 8 }
+        };
+        // Rendering = 0
+        // Audio = 1
+        // Physics = 2
+        // UI = 3
+        // Lighting = 4
+        // VFX = 5
+        // AI = 6
+        // Scripting = 7
+        // IO = 8
+        // Other = 9
+        // Video = 10
+        // Animation = 11
+        // Input = 12
         
         [MenuItem("Window/Analysis/Analyze Recorded Profiler Data", false, 50)]
-        private static void Analyze()
+        private static async void Analyze()
         {
             Path = $"Benchmark/ProfilerUtilizationData {DateTime.Now:dd-MM-yyyy HH-mm-ss}.csv";
             Files = Directory.EnumerateFiles("Benchmark/").Where(file => file.Contains("ProfilerData") && file.Contains(".raw")).ToList();
             Files.Sort();
             FileIndex = 0;
-            HeaderWritten = false;
             FrameIndex = 0;
             Profiler.maxUsedMemory = int.MaxValue;
             ProfilerDriver.profileLoaded += AnalyzeFile;
+            const string header = "Frame Index;Frame Time;Frame FPS;Rendering;Audio;Physics;UI;Lighting;VFX;AI;Scripting;IO;Other;Video;Animation;Input\r\n";
+            await File.AppendAllTextAsync(Path, header);
             ProfilerDriver.LoadProfile(Files[FileIndex], false);
         }
 
         private static async void AnalyzeFile()
         {
             Dictionary<ushort, string> categoryIdToName = new();
-            Dictionary<string, float> categoryUtilization = new();
+            float[] categoryUtilization = new float[13];
             List<ProfilerCategoryInfo> categories = new();
             for (int i = ProfilerDriver.firstFrameIndex; i <= ProfilerDriver.lastFrameIndex; i++)
             {
                 using (HierarchyFrameDataView hierarchyFrameDataView = ProfilerDriver.GetHierarchyFrameDataView(i, 0, HierarchyFrameDataView.ViewModes.InvertHierarchy, HierarchyFrameDataView.columnDontSort, false))
                 {
                     hierarchyFrameDataView.GetAllCategories(categories);
-                    if (!HeaderWritten)
-                    {
-                        string header = "Frame Index;Frame Time;Frame FPS";
-                        foreach (ProfilerCategoryInfo category in categories)
-                        {
-                            header += $";{category.name}";
-                        }
-                        header += "\r\n";
-                        await File.AppendAllTextAsync(Path, header);
-                        HeaderWritten = true;
-                    }
                     foreach (ProfilerCategoryInfo category in categories)
                     {
                         categoryIdToName.Add(category.id, category.name);
-                        categoryUtilization.Add(category.name, 0);
                     }
                     List<int> childrenIds = new();
                     hierarchyFrameDataView.GetItemChildren(0, childrenIds);
@@ -80,19 +123,26 @@ namespace PowerProfiler.Editor
                         }
                         else
                         {
-                            categoryUtilization[categoryIdToName[categoryId]] += hierarchyFrameDataView.GetItemColumnDataAsFloat(childId, HierarchyFrameDataView.columnSelfPercent);
+                            string categoryName = categoryIdToName[categoryId];
+                            bool hasMapping = CategoryMapping.TryGetValue(categoryName, out int categoryIndex);
+                            if (!hasMapping)
+                            {
+                                categoryIndex = 9;
+                                Debug.LogWarning($"Missing Category Mapping for {categoryName}");
+                            }
+                            categoryUtilization[categoryIndex] += hierarchyFrameDataView.GetItemColumnDataAsFloat(childId, HierarchyFrameDataView.columnSelfPercent);
                         }
                     }
                     FrameIndex++;
                     string content = $"{FrameIndex};{hierarchyFrameDataView.frameTimeMs:F3};{hierarchyFrameDataView.frameFps:F2}";
-                    foreach (KeyValuePair<string, float> utilization in categoryUtilization)
+                    foreach (float utilization in categoryUtilization)
                     {
-                        content += $";{utilization.Value:F2}";
+                        content += $";{utilization:F2}";
                     }
                     content += "\r\n";
                     await File.AppendAllTextAsync(Path, content);
                     categoryIdToName.Clear();
-                    categoryUtilization.Clear();
+                    categoryUtilization = new float[13];
                     categories.Clear();
                 }
             }
