3b61bba7026a8c2f42e63515e4c66fdf95fb4ccd
Julien Lukasewycz <julien.lukasewycz@uni-due.de>
2025-02-25 11:42:16 +0100

    [ADDED] ProfilerDataCollector script and functionality

---
+++ Package/Runtime/ProfilerDataCollector.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Profiling;
+
+namespace PowerProfiler.Runtime
+{
+    public class ProfilerDataCollector : MonoBehaviour
+    {
+        private int frameCount;
+        
+        private void Awake()
+        {
+            Profiler.logFile = $"Benchmark/ProfilerData {DateTime.Now:dd-MM-yyyy HH-mm-ss-ffffff}.raw";
+            Profiler.enableBinaryLog = true;
+            Profiler.enabled = true;
+        }
+
+        private void Update()
+        {
+            frameCount++;
+            if (frameCount == 300)
+            {
+                Profiler.logFile = $"Benchmark/ProfilerData {DateTime.Now:dd-MM-yyyy HH-mm-ss-ffffff}.raw";
+                Profiler.enableBinaryLog = true;
+                Profiler.enabled = true;
+                frameCount = 0;
+            }
+        }
+        
+        private void OnApplicationQuit()
+        {
+            Profiler.enabled = false;
+            Profiler.logFile = string.Empty;
+        }
+    }
+}
